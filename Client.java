082c12ff6c51676e9e1f1ccfe777bec09c4155e1
Salahuddin M. ElKazak <salahuddin.kazak@gmail.com>
2017-02-07 04:49:48 +0200

    Add comments

--- Client.java
+++ Client.java
@@ -6,26 +6,32 @@ import java.io.*;
 import java.net.*;
 
 public class Client {
-
+	//single client socket for all instances of Client
 	public static Socket skt = null;
 	chat frame;
 	//PrintWriter pw = null;
+	//constructor needs GUI JFrame
 	public Client(chat frame)
 	{
 	     try {
+		//requires IP entry, if not defaults to 192.168.0.32
 		String ip=JOptionPane.showInputDialog(null,"Please enter the IP");
+		//attempts connection establishment on socket 8085
 		if(ip==null || ip.equals("null"))
 		 skt = new Socket("192.168.0.32", 8085);
 		else skt = new Socket(ip, 8085);
+		//creates a bufferedreader of the connection's input stream
 		 BufferedReader in = new BufferedReader(new
 		    InputStreamReader(skt.getInputStream()));
-
+		//activates GUI frame
 		this.frame=frame;
 		frame.chat_form();
 		frame.enter_value_in();
 		String message="";
+		//while the message is not exit, attempt sending message
 		while(!message.equals("exit"))
 		{
+		//wait for the input stream reader to be ready
 		 while (!in.ready()) {}
 		 	frame.s=in.readLine(); // Read one line and output it
 			System.out.println(frame.s);
@@ -40,6 +46,7 @@ public class Client {
 	public static void sendMessage(String message)
 	{
 		try{
+			//get output stream and send a message through
 			PrintWriter pw=null;
 			try{pw=new PrintWriter(skt.getOutputStream());}catch(Exception ex){System.out.println(ex.getMessage());}
 			pw.println(message);
--- chat.java
+++ chat.java
@@ -5,8 +5,11 @@ import java.awt.event.ActionListener;
 import java.awt.*;
 
 public class chat extends JFrame{
+	//textfield for sending
 	JTextField t=new JTextField(150);
+	//add an enter button
 	JButton b=new JButton("enter");
+	//large textarea for messages
 	JTextArea tf=new JTextArea();
 	String m="" ;
 	boolean flag;
@@ -15,9 +18,10 @@ public class chat extends JFrame{
 	Client client;
 	
 	public void chat_form(){
-
+		//disable textarea,use CYAN
 		tf.setEditable(false);
 		tf.setSelectedTextColor(Color.CYAN);
+		//sort into familiar messenger layout
 		JPanel p2=new JPanel();
 		p2.setLayout(new BorderLayout(5,10));
 		p2.add(t,BorderLayout.CENTER);
@@ -28,12 +32,15 @@ public class chat extends JFrame{
 		
 	}
 	public void enter_value_out(){
+		//increment message with string s
 		m=m+s+"\n";
 		tf.setText(m);
 		s="";
 	}
 	public void enter_value_in(){
+		// create an object from enter
 		ActionListener w =new enter();
+		// wait for button press
 		b.addActionListener(w);
 	}
 	public  chat(){
@@ -48,13 +55,17 @@ public class chat extends JFrame{
 	private class enter implements ActionListener{
 		public void actionPerformed(ActionEvent e){
 			//m= m + t.getText()+"\n";
+			//get the text in textfield
 			String temp = t.getText();
+			//send the message through client
 			client.sendMessage(temp);
 			//tf.setText(m);
+			//empty the textfield
 			t.setText("");
 		}
 	}
 	   public static void main(String args[]) {
+		//create a chat window
 
 		chat c = new chat();
 	   }
